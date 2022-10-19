06b5e0f6a6e102f9e98c438316d22188cb5025fa
zp021 <111681672+zp021@users.noreply.github.com>
2022-10-19 20:48:54 +0200

    css changes and js functionality commit

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ const backButton = document.getElementById("back-button")
 const ratings = document.querySelectorAll(".btn")
 const userRating = document.getElementById("rating")
 
+window.addEventListener("load", () => {
+  submitButton.disabled = true
+})
+
+ratings.forEach((rating) => {
+  rating.addEventListener("click", () => {
+  submitButton.disabled = false
+  userRating.innerHTML = rating.innerHTML;
+  })
+})
+
 submitButton.addEventListener("click", () => {
   ratingContainer.style.display = "none"
   thankYouContainer.classList.remove("hidden")
-
-  ratings.forEach((rating) => {
-    rating.addEventListener("click", () => {
-      userRating.innerHTML = rating.innerHTML
-    })
-  })
 })
 
 backButton.addEventListener("click", () => {
   ratingContainer.style.display = "block"
   thankYouContainer.classList.add("hidden")
+  submitButton.disabled = true
 })
